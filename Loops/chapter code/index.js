bfc255a008854d967280936812f16fc5f93e6b1a
ramansingh19 <raman.01kumar1@gmail.com>
2025-06-19 18:15:46 +0530

    add

---
+++ Loops/chapter code/index.js
@@ -0,0 +1,37 @@
+// let a;
+// for (a = 0; a<=1000; a++) {
+//   console.log(a);
+
+// }
+
+// while loop
+let a =1;
+while (a<=10) {
+  console.log(a);
+  a++;
+  
+}
+
+let x = 0;
+while (x < 2) {
+  console.log(x);
+  x = Math.random() * 3;
+}
+console.log(` your number is ${x}`);
+
+
+//Do While Loop 
+let  s = 2;
+do {
+  console.log(s);
+  s++
+  
+} while (s > 4);
+
+//for loop
+let array = [2,4,5,6,7,6,6,,5545,5,545,4,7]
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  console.log(array[index]);
+  
+}
