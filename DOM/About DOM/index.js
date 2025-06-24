231eca6706d50d2f381e563622258260137fd69c
ramansingh19 <raman.01kumar1@gmail.com>
2025-06-24 19:23:30 +0530

    DOM

---
+++ DOM/About DOM/index.js
@@ -0,0 +1,11 @@
+console.log(document.getElementsByClassName('Apna'));
+
+
+let image = document.getElementsByClassName('wolf');
+
+
+for(let i=0; i<image.length; i++){
+  console.dir(image[i]);
+  
+  
+}
