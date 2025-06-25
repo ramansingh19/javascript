b8de2a3839e7e7b372d8f0ee67b048c371de3785
ramansingh19 <raman.01kumar1@gmail.com>
2025-06-25 17:43:42 +0530

    DOM

--- DOM/About DOM/index.js
+++ DOM/About DOM/index.js
@@ -1,20 +1,60 @@
-console.log(document.getElementsByClassName('Apna'));
+// console.log(document.getElementsByClassName('Apna'));
 
 
-let image = document.getElementsByClassName('wolf');
+// let image = document.getElementsByClassName('wolf');
 
 
-for(let i=0; i<image.length; i++){
-  console.dir(image[i]);
+// for(let i=0; i<image.length; i++){
+//   console.dir(image[i]);
   
   
-}
+// }
 
-let heading = document.querySelector('.name');
-heading.innerHTML = 'Aman Kumar';
-heading.innerHTML = '<b>Aman Kumar</b>';
-heading.innerHTML = '<u>Aman Kumar</u>';
+// let heading = document.querySelector('.name');
+// heading.innerHTML = 'Aman Kumar';
+// heading.innerHTML = '<b>Aman Kumar</b>';
+// heading.innerHTML = '<u>Aman Kumar</u>';
+
+// //styling 
+// let heading1 = document.querySelector('.green');
+
+// heading1.classList.add("pink");
+// console.log(heading1.classList);
+
+// //toggle
+// let heading2 = document.querySelector('.green');
+
+// heading2.classList.toggle("pink");
+// console.log(heading1.classList);
+
+// //Navigation
+// let p = document.querySelector('p');
+// console.log(p.parentElement);//parent element
+
+// let h1 = document.querySelector('h1');
+// // console.log(h1.childElementCount);//childelement
 
 
+// //Adding element
+
+// let newp = document.createElement('newp')
+// newp.innerText = 'Hi! I am Raman Kumar, '
+
+// let body = document.querySelector('body');
+// console.log(body.appendChild(newp));
+
+
+// //insertAdjacentElement
+// let btn = document.createElement('button')
+// btn.innerText = 'Click ME,'
+
+// let h1 = document.querySelector('h1');
+// console.log(body.appendChild(btn));
+
+// h1.insertAdjacentElement("afterend", btn)
+
+
+
+//Practice Question
 
 
