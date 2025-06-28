d3d48bc96b3e5e9533425eea01f7cf436b1dcda6
ramansingh19 <raman.01kumar1@gmail.com>
2025-06-28 11:59:35 +0530

    more events'

---
+++ About DOM/DOM Events/More event/index.js
@@ -0,0 +1,7 @@
+let user = document.querySelector("#user");
+let p = document.querySelector("p");
+
+user.addEventListener("input", function() {
+  console.log(user.value);
+  p.innerHTML = user.value;
+})
