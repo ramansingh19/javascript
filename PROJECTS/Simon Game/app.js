999eb40991f6dffb8ff9d1eceb8a932bcfd87926
ramansingh19 <raman.01kumar1@gmail.com>
2025-07-10 10:48:59 +0530

    game completed

--- PROJECTS/Simon Game/app.js
+++ PROJECTS/Simon Game/app.js
@@ -6,7 +6,38 @@ let started = false;
 
 let btns = ["red", "pink", "blue", "viloet"]
 
-let h2 = document.querySelector("h2")
+let h2 = document.querySelector("h2");
+
+let speed = 1000 - level * 50;
+
+let highScore = localStorage.getItem("highScore") || 0;
+
+document.getElementById("themeBtn").addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
+
+let difficulty = "easy";
+document.getElementById("difficulty").addEventListener("change", function() {
+  difficulty = this.value;
+  if (difficulty === "hard") {
+    strictMode = true;
+    speed = 300;
+  } else {
+    strictMode = false;
+    speed = 700;
+  }
+});
+
+let soundOn = true;
+document.getElementById("soundToggle").addEventListener("change", function() {
+  soundOn = this.checked;
+});
+
+function playSound(color) {
+  if (!soundOn) return;
+  let audio = new Audio(`sounds/${color}.mp3`);
+  audio.play();
+}
 
 document.addEventListener("keypress", function() {
   if(started == false){
@@ -28,59 +59,56 @@ function userflash(btn) {
   btn.classList.add("userflash");
   setTimeout(function () {
     btn.classList.remove("userflash");
-  }, 250);
+  }, Math.max(100, 300 - level * 10));//flash faster when level will be upgrade 
 }
 
 function levelUp() {
   user = [];
   level++;
   h2.innerText = `Level ${level}`;
-
-  let randIdx = Math.floor(Math.random() * 3) + 1;
+  let randIdx = Math.floor(Math.random() * btns.length);
   let randcolor = btns[randIdx];
   let randBtn = document.querySelector(`.${randcolor}`);
   gameSeq.push(randcolor);
   console.log(gameSeq);
-  
   gameflash(randBtn);
 }
 
 function checkAns(idx) {
   // console.log(`Current lebel : ${level}`);
-  
-
   if(gameSeq[idx] === user[idx]) {
     if (user.length == gameSeq.length) {
       setTimeout(levelUp ,1000);
     }
   }else {
-    h2.innerHTML = `game over! Your score is <b>${level}</b> <br>Press any key to start`;
-
+    h2.innerHTML = `Game Over! Your score: <b>${level}</b> <br>High Score: <b>${highScore}</b> <br>Press any key to restart`;
     document.querySelector("body").style.backgroundColor = "red";
     setTimeout(function() {
       document.querySelector("body").style.backgroundColor = "white";
     }, 250);
     reset();
   }
-  
-  
+  if (level > highScore) {
+  highScore = level;
+  localStorage.setItem("highScore", highScore);
+}
 }
 
 function btnPress() {
   let btn = this;
   userflash(btn);
-
   userColor = btn.getAttribute("id");
   user.push(userColor);
-
   checkAns(user.length-1);
   
 }
+
 
 let allbtns = document.querySelectorAll(".btn");
 for(btn of allbtns){
   btn.addEventListener("click", btnPress);
 }
+
 
 function reset() {
   started = false;
@@ -88,3 +116,4 @@ function reset() {
   level = 0;
   user = [];
 }
+
