39e3282e8bb9522a12d549ca8d9f088acc98bddc
ramansingh19 <raman.01kumar1@gmail.com>
2025-08-27 18:37:05 +0530

    date and time

---
+++ Date__Time/app.js
@@ -0,0 +1,11 @@
+let mydate  = new Date();
+
+//Methodsgit 
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toTimeString());
+console.log(mydate.toUTCString());
