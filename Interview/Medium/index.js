81fa8149d67075b7967abb6a5077f2e0097fdafd
ramansingh19 <raman.01kumar1@gmail.com>
2025-12-17 12:42:45 +0530

    ss

--- Interview/Medium/index.js
+++ Interview/Medium/index.js
@@ -1,154 +1,170 @@
-//01
-// console.log("Value of the Age" , age);
-// var age = 20;
-// console.log("value of the age ",age);
-//ans --> 20
+// //01
+// // console.log("Value of the Age" , age);
+// // var age = 20;
+// // console.log("value of the age ",age);
+// //ans --> 20
 
-//02 
-// age = 100;
-// //console.log("age ", age);
-// let age = 30;
-//error
+// //02 
+// // age = 100;
+// // //console.log("age ", age);
+// // let age = 30;
+// //error
 
-//03
-myfun();
+// //03
+// myfun();
 
-var myfun = function () {
-  console.log("first");
+// var myfun = function () {
+//   console.log("first");
   
-}
+// }
 
- myfun();
+//  myfun();
 
-function myfun() {
-  console.log('second');
+// function myfun() {
+//   console.log('second');
   
-}
- myfun();
- //second , first , first 
+// }
+//  myfun();
+//  //second , first , first 
 
 
- //04
-//  var variable = 10;
+//  //04
+// //  var variable = 10;
 
-//  (() => {
-//   console.log(variable);
+// //  (() => {
+// //   console.log(variable);
 
+// //   variable = 20;
+  
+// //   console.log(variable);
+  
+  
+// //  })();
+
+// //  var variable = 30;
+
+// // console.log(variable);
+
+// //05
+// // foo = 40;
+// // console.log("foo", foo);
+
+// // var foo = 100;
+// // console.log("foo", foo);
+
+
+// //06
+// variable = 10;
+
+// (() => {
+//   foo = 100;
+//   //console.log(variable);
+//   var foo = 100;
 //   variable = 20;
-  
-//   console.log(variable);
-  
-  
-//  })();
-
-//  var variable = 30;
-
-// console.log(variable);
-
-//05
-// foo = 40;
-// console.log("foo", foo);
-
-// var foo = 100;
-// console.log("foo", foo);
-
-
-//06
-variable = 10;
-
-(() => {
-  foo = 100;
-  //console.log(variable);
-  var foo = 100;
-  variable = 20;
-  //console.log(variable);
+//   //console.log(variable);
   
   
-})();
+// })();
 
-//onsole.log(foo);
-//console.log(variable);
-var variable = 30;
+// //onsole.log(foo);
+// //console.log(variable);
+// var variable = 30;
 
 
-// //07
-// for(var i=0; i<=10; i++){
-//   setTimeout(() => console.log(i) , 0);
-// }
+// // //07
+// // for(var i=0; i<=10; i++){
+// //   setTimeout(() => console.log(i) , 0);
+// // }
 
-//08
-//var fullname = "piyush"
+// //08
+// //var fullname = "piyush"
 
-// var obj = {
-//   fullname : "Hacked ",
+// // var obj = {
+// //   fullname : "Hacked ",
 
-//   prop : {
-//     fullname : 'inside ',
-//     getfullname : function () {
-//       return this.fullname
-//     },
-//   },
+// //   prop : {
+// //     fullname : 'inside ',
+// //     getfullname : function () {
+// //       return this.fullname
+// //     },
+// //   },
 
-//   getfullname : function(){
-//     return this.fullname;
-//   },
+// //   getfullname : function(){
+// //     return this.fullname;
+// //   },
 
-//   getfullnamev2 : () => this.fullname,
+// //   getfullnamev2 : () => this.fullname,
 
-//   getfullnamev3 : (function  () {
-//     return this.fullname
-//   })(),
-// };
+// //   getfullnamev3 : (function  () {
+// //     return this.fullname
+// //   })(),
+// // };
 
-// console.log(obj.prop.getfullname);
-// console.log(obj.getfullname);
-// console.log(obj.getfullnamev2);
-// console.log(obj.getfullnamev3);
+// // console.log(obj.prop.getfullname);
+// // console.log(obj.getfullname);
+// // console.log(obj.getfullnamev2);
+// // console.log(obj.getfullnamev3);
 
-//09
-// const piyush = {
-//   name : 'piyush garg',
-//   sayName : function() {
-//     //console.log(this.name);
+// //09
+// // const piyush = {
+// //   name : 'piyush garg',
+// //   sayName : function() {
+// //     //console.log(this.name);
     
-//   }
-// }
+// //   }
+// // }
 
-// const john = {
-//   name : 'john garg',
-//   sayName : function() {
-//    // console.log(this.name);
+// // const john = {
+// //   name : 'john garg',
+// //   sayName : function() {
+// //    // console.log(this.name);
     
-//   }
-// }
+// //   }
+// // }
 
-// john.sayName.call(piyush);
+// // john.sayName.call(piyush);
 
-//10 
+// //10 
 
-const piyush = {
-  name : 'piyush garg',
-  sayName : function() {
-    console.log(this.name);
+// // const piyush = {
+// //   name : 'piyush garg',
+// //   sayName : function() {
+// //     console.log(this.name);
     
-  }
-}
+// //   }
+// // }
 
-setTimeout (() => piyush.sayName() , 3 * 1000);
+// // setTimeout (() => piyush.sayName() , 3 * 1000);
 
 
-//11
-// const obj = {
-//   height : 30,
-// }
-// console.log(obj.height);
-// delete  obj.height
-// console.log(obj.height);
+// //11
+// // const obj = {
+// //   height : 30,
+// // }
+// // console.log(obj.height);
+// // delete  obj.height
+// // console.log(obj.height);
 
-//12
-const obj = Object.create({
-  height : 30,
-})
-console.log(obj.height);
-delete  obj.height
-console.log(obj.height);
+// // //12
+// // const obj = Object.create({
+// //   height : 30,
+// // })
+// // console.log(obj.height);
+// // delete  obj.height
+// // console.log(obj.height);
+
+// //13 map function
+// let data = [1,2,3,4,5,6];
+
+// let data2 = data.map(data => data * 2)
+// console.log(data);
+
+// console.log(data2);
+
+ //14 Event bubbling  and event capturing 
+ //go see in the html file
+
+ //15
+//  what is higher -order function
+
+ 
