7da421fba8da3e883cec4a38e19446ecccc1e660
ramansingh19 <raman.01kumar1@gmail.com>
2026-01-12 11:41:32 +0530

    isPrime , largest and smallest

--- Interview/Hard/index.js
+++ Interview/Hard/index.js
@@ -29,8 +29,46 @@ function isPrime(num){
   return true;
 
 }
-console.log(isPrime(4));
-console.log(isPrime(2));
-console.log(isPrime(17));
+// console.log(isPrime(4));
+// console.log(isPrime(2));
+// console.log(isPrime(17));
+
+function isPrimee(num) {
+    if(num <= 1) return false;
+
+    for (let i = 2; i <num; i++) {
+      if(num % i === 0){
+        return false;
+      }
+    }
+    return true;
+}
+// console.log(isPrimee(7));
+// console.log(isPrimee(2));
+
+//largest or smallest
+
+let arr4 = [1,2,3,4,5];
+
+let largest = Number.MIN_SAFE_INTEGER;
+let smallest = Number.MAX_SAFE_INTEGER;
+
+for (let i = 0; i <arr4.length; i++) {
+  
+  if (arr4[i] > largest) {
+    largest = arr4[i];
+  }
+  if (arr4[i] < smallest) {
+    smallest = arr4[i];
+    
+  }
+  
+}
+console.log(largest);
+console.log(smallest);
+
+
+
+
 
 
