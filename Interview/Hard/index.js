a94afc0c80efdc4da269d033503f219c91ecdebb
ramansingh19 <raman.01kumar1@gmail.com>
2026-01-15 14:24:13 +0530

    v

--- Interview/Hard/index.js
+++ Interview/Hard/index.js
@@ -201,20 +201,27 @@
 
 
 //second largest 
-let arr = [1,2,3,4,5];
-let largest = -Infinity;
-let secondLargest = -Infinity;
+// let arr = [1,2,3,4,5];
+// let largest = -Infinity;
+// let secondLargest = -Infinity;
 
-for(let i=0; i<arr.length; i++){
-    if (arr[i] > largest) {
-        secondLargest = largest;
-        largest = arr[i]
-    }else if (arr[i] > secondLargest && arr[i] !== largest) {
-        secondLargest = arr[i];
-    }
-}
-console.log(secondLargest);
-console.log(largest);
+// for(let i=0; i<arr.length; i++){
+//     if (arr[i] > largest) {
+//         secondLargest = largest;
+//         largest = arr[i]
+//     }else if (arr[i] > secondLargest && arr[i] !== largest) {
+//         secondLargest = arr[i];
+//     }
+// }
+// console.log(secondLargest);
+// console.log(largest);
+
+
+//palindrome
+let str = "ama"
+
+
+
 
 
 
