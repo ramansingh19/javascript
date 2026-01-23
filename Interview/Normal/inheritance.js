cbe4fc76947c8321d3f45a31b430b5e9acb48ffd
ramansingh19 <raman.01kumar1@gmail.com>
2026-01-23 12:12:45 +0530

    inheritance

---
+++ Interview/Normal/inheritance.js
@@ -0,0 +1,21 @@
+class Animal{
+  constructor(name){
+    this.name = name
+  }
+
+  speak() {
+    console.log(`${this.name} is sounds good`);
+    
+  }
+
+}
+
+class Dog extends Animal{
+  speak() {
+    console.log(`${this.name} is good`);
+    
+  }
+}
+
+let dog = new Dog("jimmy")
+dog.speak()
