e2a452f623c60a9866ccd21bddefab93fcbb1438
ramansingh19 <raman.01kumar1@gmail.com>
2026-01-26 19:35:07 +0530

    feat: add test function and sum function with argument handling

--- Interview/Hard/index.js
+++ Interview/Hard/index.js
@@ -509,3 +509,21 @@
 // console.log(a2.x);
 // Each object connects to the prototype at the time it is created.
 // Changing A.prototype later does NOT update old objects.
+
+//27
+function test(a,b){
+arguments[0]=10;
+console.log(a);
+
+}
+test(5,6);
+
+function sum() {
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
+}
+
+console.log(sum(1,2,3));
